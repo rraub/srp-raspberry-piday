a4aabe2d9dcbeda9585bb3eb0be0c5e06c9b42d5
Ryan Raub <ryan.raub@gmail.com>
2019-03-12 21:03:19 -0700

    ignore datafiles, add comments

--- etc/start.sh
+++ etc/start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 python /home/pi/srp/piday/piday.py >> /home/pi/srp/piday/output.csv
 
+# this file is to be symlinked into /etc/init.d/start-piday
+# Also `sudo chmod 755 start-piday`
+# and finally `sudo update-rc.d start-piday defaults`
