46e22ed3844b654983a02080ec41a1fc5ee675a2
Your Name <you@example.com>
2019-04-22 21:04:53 -0700

    adding wifi info

--- etc/raspberrypi-stats.sh
+++ etc/raspberrypi-stats.sh
@@ -4,6 +4,7 @@ date=`date`
 temp=`vcgencmd measure_temp`
 volts=`vcgencmd measure_volts core`
 clock_freq=`vcgencmd measure_clock arm`
-echo "$date $temp $volts $clock_freq"
+wifi_info=`iwconfig wlan0`
+echo "$date $temp $volts $clock_freq $wifi_info"
 
 
