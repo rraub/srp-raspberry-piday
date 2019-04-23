e2c2b4c9b189f3996339e2bdd63e8b0f1113c451
Your Name <you@example.com>
2019-04-22 20:58:33 -0700

    self update crontab

--- etc/update-piday.sh
+++ etc/update-piday.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 date
 cd /home/pi/srp/piday 
 git pull origin master
+
+# update cron tab
+crontab $DIR/piday.cron
 
 
