9baccfe32dae37f403d85329d481da1db88ccaf6
Ryan Raub <ryan.raub@gmail.com>
2019-06-04 15:38:33 -0700

    stop recording wifi info

--- etc/raspberrypi-stats.sh
+++ etc/raspberrypi-stats.sh
@@ -4,7 +4,6 @@ date=`date`
 temp=`vcgencmd measure_temp`
 volts=`vcgencmd measure_volts core`
 clock_freq=`vcgencmd measure_clock arm`
-wifi_info=`/sbin/iwconfig wlan0`
-echo "$date $temp $volts $clock_freq $wifi_info"
+echo "$date $temp $volts $clock_freq"
 
 
